3fda1b7f2db67f15492320c6b92b690557be4b54
Hao Nguyen <32004536+nguyenanhhao221@users.noreply.github.com>
2022-09-03 16:27:41 +0700

    problem 1 to 6

--- src/02-object-param.problem.ts
+++ src/02-object-param.problem.ts
@@ -1,21 +1,25 @@
-import { expect, it } from "vitest";
+import { expect, it } from 'vitest';
+type ParamsTypes = {
+  first: number;
+  second: number;
+};
 
-export const addTwoNumbers = (params) => {
+export const addTwoNumbers = (params: ParamsTypes) => {
   return params.first + params.second;
 };
 
-it("Should add the two numbers together", () => {
+it('Should add the two numbers together', () => {
   expect(
     addTwoNumbers({
       first: 2,
       second: 4,
-    }),
+    })
   ).toEqual(6);
 
   expect(
     addTwoNumbers({
       first: 10,
       second: 20,
-    }),
+    })
   ).toEqual(30);
 });
